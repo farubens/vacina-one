7add02fb1dd5fe671ca9833075c355578e4db2fb
farubens <farubens@users.noreply.github.com>
2026-06-09 12:44:02 -0300

    Add Meta domain verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <meta name="facebook-domain-verification" content="rkxgxmjrftaegqf8tmjv6znwhpdl81" />
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
